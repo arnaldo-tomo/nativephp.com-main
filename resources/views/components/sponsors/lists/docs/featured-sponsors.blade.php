ed11777c5a60742de690be9f957ee02d93945c98
Arnaldo tomo <arnaldotomo@gmail.com>
2025-08-07 09:43:44 +0200

    Update featured-sponsors.blade.php

--- resources/views/components/sponsors/lists/docs/featured-sponsors.blade.php
+++ resources/views/components/sponsors/lists/docs/featured-sponsors.blade.php
@@ -1,12 +1,12 @@
 <a
-    href="https://beyondco.de/?utm_source=nativephp&utm_medium=logo&utm_campaign=nativephp"
+    href="https://arkosjs.com/"
     title="Learn more about BeyondCode"
     aria-label="Visit BeyondCode website"
     class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
     rel="noopener sponsored"
 >
     <img
-        src="/img/sponsors/beyondcode.webp"
+        src="https://arkosjs.com/img/arkos-js-logo-dark.svg"
         alt="BeyondCode company logo - featured sponsor"
         class="block h-auto max-h-8 max-w-full dark:hidden"
         loading="lazy"
@@ -14,18 +14,43 @@
         height="32"
     />
     <img
-        src="/img/sponsors/beyondcode-dark.webp"
+        src="https://arkosjs.com/img/arkos-js-logo-dark.svg"
         alt="BeyondCode company logo - featured sponsor"
         class="hidden h-auto max-h-8 max-w-full dark:block"
         loading="lazy"
         width="128"
         height="32"
     />
-    <span class="sr-only">BeyondCode</span>
+    <span class="sr-only">Arkos.js</span>
+</a>
+<a
+    href="https://superm7.com/"
+    title="Learn more about BeyondCode"
+    aria-label="Visit BeyondCode website"
+    class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
+    rel="noopener sponsored"
+>
+    <img
+        src="https://arkosjs.com/img/arkos-js-logo-dark.svg"
+        alt="BeyondCode company logo - featured sponsor"
+        class="block h-auto max-h-8 max-w-full dark:hidden"
+        loading="lazy"
+        width="128"
+        height="32"
+    />
+    <img
+        src="https://superm7.com/_next/image?url=%2Fimages%2Flogo.png&w=256&q=75"
+        alt="BeyondCode company logo - featured sponsor"
+        class="hidden h-auto max-h-8 max-w-full dark:block"
+        loading="lazy"
+        width="128"
+        height="32"
+    />
+    <span class="sr-only">Superm7</span>
 </a>
 
-<a
-    href="https://laradevs.com/?ref=nativephp"
+{{-- <a
+    href="https://superm7.com/"
     title="Learn more about Laradevs"
     aria-label="Visit Laradevs website"
     class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
@@ -35,5 +60,5 @@
         class="block h-auto max-h-8 max-w-full text-black dark:text-white"
         aria-hidden="true"
     />
-    <span class="sr-only">Laradevs</span>
-</a>
+    <span class="sr-only">superm7</span>
+</a> --}}
