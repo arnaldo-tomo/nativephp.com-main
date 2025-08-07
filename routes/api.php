50f2c3dd14ebb09784c27dfd35cc01afdb71ff5b
Arnaldo tomo <arnaldotomo@gmail.com>
2025-08-07 10:22:01 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\Api\LicenseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// routes/web.php
+Route::get('/api/search-index', [App\Http\Controllers\SearchController::class, 'index']);
+Route::get('/api/search', [App\Http\Controllers\SearchController::class, 'search']);
